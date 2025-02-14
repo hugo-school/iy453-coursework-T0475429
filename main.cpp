f898d055874b7dda453c725b918c7c8b32159805
hugochan <T0475429@my.ntu.ac.uk>
2025-02-14 10:56:51 +0000

    453 code part1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int choice;
+    cout << "Welcome to play brave adventure. \nIn this game all you decision will affect to the story. \npress 1 to start the game" <<endl;
+    cin >> choice;
+    if (choice == 1) {
+        cout << "you turn to 18 today, now you become a \n1 Swordsman \n2 Magician" << endl;
+        cin >> choice;
+        if (choice == 1) {
+            cout << "You choose to become a Swordsman" << endl;
+        }
+    }
+
+}
+
